35a2cfad5e1890fac98b73a64ba0600b7484a427
devoil <devdevoildev@gmail.com>
2025-09-04 21:49:00 +0900

    refactor: [lecture 1] bottleneck 코드 분석 및 최적화

--- lecture-1/src/components/Article/index.js
+++ lecture-1/src/components/Article/index.js
@@ -16,22 +16,14 @@ function getParametersForUnsplash({width, height, quality, format}) {
  * 파라미터로 넘어온 문자열에서 일부 특수문자를 제거하는 함수
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
+// bottleneck 해결 방안
+// 1. 특수 문자 효율적으로 제거하기
+  // 1-1. replace 함수와 정규식 사용
+  // 1-2. 마크다운의 특수문자를 지워주는 라이브러리 사용 (remove-markdown 등)
+// 2. 작업하는 양 줄이기
+  // 2-1. 최대 9만자의 내용을 다 검사할 필요 없다. 화면에 최대 2~300자 정도만 보여지는데 그정도만 검사해주면 된다.
 function removeSpecialCharacter(str) {
-  const removeCharacters = ['#', '_', '*', '~', '&', ';', '!', '[', ']', '`', '>', '\n', '=', '-']
-  let _str = str
-  let i = 0,
-    j = 0
-
-  for (i = 0; i < removeCharacters.length; i++) {
-    j = 0
-    while (j < _str.length) {
-      if (_str[j] === removeCharacters[i]) {
-        _str = _str.substring(0, j).concat(_str.substring(j + 1))
-        continue
-      }
-      j++
-    }
-  }
+  let _str = str.substring(0, 300).replace(/[#_*~&;!\[\]`>\n=\-]/g, '');
 
   return _str
 }
